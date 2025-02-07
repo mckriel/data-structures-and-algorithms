6b0c12dcf14788c3e7bc2a0f336fc75b28380925
mckriel <mckriel@gmail.com>
2025-02-07 10:48:53 +0200

    hackerrank 1 problem basic

---
+++ hacker-rank/problem-solving/1.js
@@ -0,0 +1,10 @@
+// compare triplets
+let result = [0, 0];
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] > b[i]) {
+            result[0] += 1;
+        } else if (a[i] < b[i]) {
+            result[1] += 1;
+        }
+    }
+    return result;
